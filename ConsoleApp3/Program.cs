2b67a5cef1435e3efeb4416c263a15128f3a512c
asus <asus@LAPTOP-U90DK29T>
2021-12-03 10:58:27 +0700

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //check apakah bilangan prima atau bukan
+            Console.Write(“masukan bilangan bulat = ”);
+            bil = Convert.ToInt16(Console.ReadLine());
+            jumlahbilangan = 0;
+            for (ulang = 1; ulang <= bil; ulang++)
+        }
+
+    }
+}
